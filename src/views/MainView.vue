b02981059436b05ccf7914c0ca1327de5a5cbe1f
Tonyson <dongo824@naver.com>
2022-11-29 14:45:09 +0900

    Complete header
    MainView - header

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,45 +1,47 @@
 <template>
-  <div class="md:bg-[#fafafa]">
+  <div>
     <!-- 헤더 -->
-    <div class="cursor-default w-full fixed top-0 z-50">
-      <!-- 웹 헤더 -->
-      <div class="px-4 py-4 hidden md:block bg-[#fafafa] w-full">
-        <div class="flex">
-          <div class="lg:flex-1"></div>
-          <div
-            class="flex justify-between items-center w-[1180px] lg:flex-shrink"
-          >
-            <div class="hidden md:block">
-              <img src="@/assets/icon/logo_vertical.svg" alt="" />
-            </div>
-            <div class="hidden md:block">
-              <div class="flex space-x-4 items-center">
-                <img src="@/assets/icon/chat_mid-grey.svg" alt="" />
-                <img src="@/assets/icon/notify_mid-grey.svg" alt="" />
-                <img src="@/assets/icon/profile_mid-grey.svg" alt="" />
-              </div>
+
+    <!-- 웹 헤더  -->
+    <div class="flex cursor-default bg-[#fafafa] inline-block top-0 z-50">
+      <div class="flex-1 hidden md:block"></div>
+      <div class="flex-none hidden md:block">
+        <div
+          class="flex-none flex justify-between items-center bg-[#fafafa] w-[1180px] px-4 py-4"
+        >
+          <div class="hidden md:block">
+            <img src="@/assets/icon/logo_vertical.svg" alt="" />
+          </div>
+          <div class="hidden md:block">
+            <div class="flex space-x-4 items-center">
+              <img src="@/assets/icon/chat_mid-grey.svg" alt="" />
+              <img src="@/assets/icon/notify_mid-grey.svg" alt="" />
+              <img src="@/assets/icon/profile_mid-grey.svg" alt="" />
             </div>
           </div>
-          <div class="lg:flex-1"></div>
         </div>
       </div>
-      <!-- 모바일 헤더 -->
-      <div class="w-full bg-everly-main px-4 py-3 md:hidden">
-        <div class="flex justify-between items-center">
-          <div class="text-white font-bold">로그인하기</div>
-          <div class="md:hidden">
-            <div class="flex space-x-4">
-              <img src="@/assets/icon/filter_white.svg" alt="" />
-              <img src="@/assets/icon/search_white.svg" alt="" />
-              <img src="@/assets/icon/notify_white.svg" alt="" />
-            </div>
+      <div class="flex-1 hidden md:block"></div>
+    </div>
+
+    <!-- 모바일 헤더 -->
+    <div
+      class="w-full bg-everly-main px-4 py-3 cursor-default fixed top-0 z-50 md:hidden"
+    >
+      <div class="flex justify-between items-center">
+        <div class="text-white font-bold">로그인하기</div>
+        <div class="md:hidden">
+          <div class="flex space-x-4">
+            <img src="@/assets/icon/filter_white.svg" alt="" />
+            <img src="@/assets/icon/search_white.svg" alt="" />
+            <img src="@/assets/icon/notify_white.svg" alt="" />
           </div>
         </div>
       </div>
     </div>
     <!-- 헤더끝 -->
 
-    <router-view class="w-full m-auto max-w-lg items-center justify-center">
+    <router-view class="w-full m-auto max-w-md items-center justify-center">
     </router-view>
     <div class="p-4 text-center w-full fixed bottom-0">
       <div
