8b2023d79579bade752fb1d6794e0a02c8af495e
gwangho-y <yigwangho21@gmail.com>
2023-01-19 14:27:35 +0900

    [U] 메세지 박스 크기 아래로 증가

--- src/components/chat/components/myMessage.vue
+++ src/components/chat/components/myMessage.vue
@@ -1,12 +1,15 @@
 <template>
    
     <div class="w-full px-5 mt-5">
-         <div class="flex items-end">
-            <!-- 채팅 시간 -->
-             <p class="ml-auto mr-3 text-xs text-everly-dark_grey">오후 06:00</p>
-             <!-- 채팅 내용 -->
-             <p class="text-sm mr-3 px-3 py-2 h-fit  md:max-w-md  rounded-lg shadow-md bg-everly-main text-everly-white">{{ props.message.text }}</p>
+         <div class="flex">
+            <div class="ml-auto flex items-end">
+                <!-- 채팅 시간 -->
+                <p class=" mr-3 text-xs text-everly-dark_grey">오후 06:00</p>
+                <!-- 채팅 내용 -->
+                <p class="text-sm mr-3 px-3 py-2 h-fit  md:max-w-md  rounded-lg shadow-md bg-everly-main text-everly-white">{{ props.message.text }}</p>
            
+            </div>
+            
              <!-- 프로필 사진 -->
              <div class="rounded-lg overflow-hidden mr-3 min-w-[2.5rem]">                           
                  <img
