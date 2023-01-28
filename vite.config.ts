d506a91f1718781929072da36f0a6da523dd48b7
Tonyson <dongo824@naver.com>
2023-01-28 11:56:11 +0900

    [H]개발 환경에 따라 빌드 타입 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ command, mode, ssrBuild }) => {
-  if (command === "serve") {
+  console.log(">>>>>>>>>>>>", mode);
+  if (mode == "production") {
     return {
       plugins: [vue()],
       resolve: {
