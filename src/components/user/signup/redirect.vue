e6b04adaafdf40fd91aa037e0def8b201f15c869
Tonyson <dongo824@naver.com>
2023-02-06 13:53:07 +0900

    [U]회원가입 나이스 인증 응답값 수정

--- src/components/user/signup/redirect.vue
+++ src/components/user/signup/redirect.vue
@@ -8,23 +8,29 @@ import { useRoute } from "vue-router";
 const route = useRoute();
 
 let authId = route.query.authId;
-let mode = route.query.mode;
+let type = route.query.type;
 let result = route.query.result;
-console.log(result);
-console.log(authId);
 
-if (result == "success") {
-  // let url = `http://localhost:5173/account/signUp/setinfo?authId=` + authId;
-  let url =
-    `https://www.item1004.co.kr/account/signUp/setinfo?authId=` + authId;
-  window.opener.location.replace(url); //부모창 reload
-  window.close(); //현재 팝업창 Close
+// 회원가입 시 사용
+if (type == "signup") {
+  if (result == "success") {
+    // let url = `http://localhost:5173/account/signUp/setinfo?authId=` + authId;
+    let url =
+      `https://www.item1004.co.kr/account/signUp/setinfo?authId=` + authId;
+    window.opener.location.replace(url); //부모창 reload
+    window.close(); //현재 팝업창 Close
+  } else {
+    failed();
+  }
 } else {
+  failed();
+}
+
+function failed() {
   alert(
     "본인인증에 실패했습니다.\n아이폰 또는 사파리 사용 중이실 경우 웹사이트를 이용해주세요."
   );
-  let url = `https://www.item1004.co.kr/account/signUp/confirm` + authId;
-  window.opener.location.replace(url); //부모창 reload
+  window.opener.location.reload(); //부모창 reload
   window.close(); //현재 팝업창 Close
 }
 </script>
