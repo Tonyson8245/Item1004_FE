81cec4405ea1e695055b13bc0d72d0279748386a
Tonyson <dongo824@naver.com>
2023-02-07 14:03:50 +0900

    [U] 회원탈퇴 개발 완료
    - UI 개발 완료
    - API 연결 완료

--- src/components/mypage/modaldetail/deleteUserAccount.vue
+++ src/components/mypage/modaldetail/deleteUserAccount.vue
@@ -1,82 +1,145 @@
 <template>
   <div
-    class="w-full md:w-[51.875rem] space-y-3 bg-everly-light_grey md:bg-everly-white md:py-2"
+    class="w-full md:w-[51.875rem] space-y-3 bg-everly-light_grey md:bg-everly-white md:py-4 md:rounded-lg"
   >
-    <div class="bg-everly-white py-7 md:py-3 px-4">
-      <div class="text-sm md:text-xl font-bold flex items-center">
-        회원탈퇴
-        <span class="hidden md:block ml-2 text-base"
-          >(회원 탈퇴 사유를 선택해 주세요. 중복 가능)</span
+    <div class="bg-everly-white py-7 md:py-1 px-4 md:px-5">
+      <div
+        class="text-sm md:text-xl font-bold flex items-center justify-between"
+      >
+        <div class="flex items-center">
+          회원탈퇴
+          <span class="hidden md:block ml-2 text-base"
+            >(회원 탈퇴 사유를 선택해 주세요. 중복 가능)</span
+          >
+          <span class="md:hidden"> 사유를 최소 1개 이상 선택해주세요 </span>
+        </div>
+        <img
+          src="@/assets/icon/close_grey.svg"
+          alt=""
+          class="hidden md:block"
+          @click="emit('update:propsShowModal', false)"
+        />
+      </div>
+      <div
+        class="grid md:grid-cols-2 gap-3 md:gap-5 text-everly-dark_grey md:text-everly-black md:text-base text-sm py-3"
+      >
+        <div
+          v-for="reason in reasonList"
+          :key="reason"
+          class="flex items-start"
         >
-        <span class="md:hidden"> 사유를 최소 1개 이상 선택해주세요 </span>
+          <div>
+            <label>
+              <input
+                class="radioinput"
+                type="checkbox"
+                :id="reason"
+                :value="reason"
+                v-model="checkedReason"
+              />
+              <span />
+            </label>
+          </div>
+          <span class="pt-0.5 md:pt-0 md:pl-1">
+            {{ reason }}
+          </span>
+        </div>
+        <div>
+          <label>
+            <input
+              class="radioinput"
+              type="checkbox"
+              id="checkbox"
+              v-model="isActiveEtc"
+            />
+            <span />
+          </label>
+          <span class="pt-0.5 md:pt-0 md:pl-1">기타</span>
+        </div>
       </div>
-      <div class="grid md:grid-cols-2 text-everly-dark_grey text-sm py-2">
-        <div>선택지 1</div>
-        <div>선택지 2</div>
-        <div>선택지 3</div>
-        <div>선택지 4</div>
-        <div>선택지 5</div>
-        <div>선택지 6</div>
-      </div>
-      <div class="text-xs border-everly-mid_grey border">
+      <div
+        class="text-xs border-everly-mid_grey border"
+        :class="textareaClass(isActiveEtc)"
+      >
         <textarea
           name=""
           id=""
           rows="3"
           placeholder="회원탈퇴 사유를 적어주세요"
-          class="p-1 w-full"
+          class="p-1 w-full h-[6.313rem]"
+          style="resize: none"
+          :disabled="!isActiveEtc"
+          v-model="etcContent"
+          @input="(event: Event) => {inserttoetcContent(event)}"
         ></textarea>
-        <div class="border-everly-dark_grey text-right">(0/200자)</div>
+        <div class="border-everly-dark_grey text-right">
+          ({{ etcContentLength }}/200자)
+        </div>
       </div>
     </div>
     <hr class="border-everly-light_grey border hidden md:block mx-4" />
-    <div class="bg-everly-white py-7 md:py-3 px-4 space-y-2">
+    <div class="bg-everly-white py-7 md:py-1 px-4 md:px-5 space-y-2">
       <div class="text-sm md:text-base font-bold">회원탈퇴 안내사항</div>
       <div
         class="text-xs md:text-sm md:text-everly-black space-y-2 text-everly-dark_grey"
       >
-        <div>
-          <div>
-            1. 탈퇴 신청 후 14일 까지는 탈퇴가 유예됩니다. 만약 유예기간 내에
-            다시 로그인할 경우 기존 회원정보는 유지되며 탈퇴 신청 이전과
-            동일하게 사용 가능합니다.
-          </div>
+        <div class="flex">
+          1.
+          <span class="block pl-1">
+            탈퇴 신청 후 14일 까지는 탈퇴가 유예됩니다. 만약 유예기간 내에 다시
+            로그인할 경우 기존 회원정보는 유지되며 탈퇴 신청 이전과 동일하게
+            사용 가능합니다.
+          </span>
         </div>
-        <div>
-          2. 탈퇴 신청 14일 이후에 탈퇴 처리 시 사용자의 데이터는 아래와 같이
-          처리됩니다.
-          <div>
-            -회원가입 시 제공하신 개인 정보 (이메일, 나이, 생일)는 영구
-            삭제됩니다.
-          </div>
-          <div>
-            -작성하신 거래글과 닉네임은 유지됩니다. ( 위 정보가 남지 않기를
-            원하는 사용자는 각 데이터를 삭제 후 탈퇴 신청을 하시기 바랍니다.)
-          </div>
+        <div class="flex">
+          2.
+          <span class="block pl-1">
+            탈퇴 신청 14일 이후에 탈퇴 처리 시 사용자의 데이터는 아래와 같이
+            처리됩니다.
+            <span class="block pl-1">
+              - 회원가입 시 제공하신 개인 정보 (이메일, 나이, 생일)는 영구
+              삭제됩니다.
+            </span>
+            <span class="block pl-1">
+              - 작성하신 거래글과 닉네임은 유지됩니다. ( 위 정보가 남지 않기를
+              원하는 사용자는 각 데이터를 삭제 후 탈퇴 신청을 하시기 바랍니다.)
+            </span>
+          </span>
         </div>
-        <div>3. 탈퇴 처리 후 재 회원가입 시 신규 사용자로 가입됩니다.</div>
+        <div class="flex">
+          3.
+          <span class="block pl-1"
+            >탈퇴 처리 후 재 회원가입 시 신규 사용자로 가입됩니다.</span
+          >
+        </div>
       </div>
     </div>
     <hr class="border-everly-light_grey border hidden md:block mx-4" />
 
-    <div class="bg-everly-white py-7 md:py-3 px-4 space-y-3">
+    <div class="bg-everly-white py-7 md:py-1 px-4 md:px-5 space-y-3">
       <div class="text-sm md:text-base font-bold">회원탈퇴 동의</div>
       <div class="text-xs flex md:text-base">
         <label>
           <input
             class="radioinput"
             type="checkbox"
-            :id="`최종선택`"
-            :v-model="withdrawlConfirm"
-            :value="`최종선택`"
+            id="checkbox"
+            v-model="withdrawlConfirm"
           />
+
           <span />
         </label>
-        회원 탈퇴 안내를 모두 확인하였으며 탈퇴에 동의합니다.
+        <span class="md:pl-1">
+          회원 탈퇴 안내를 모두 확인하였으며 탈퇴에 동의합니다.
+        </span>
       </div>
       <div class="md:px-56 space-y-2">
-        <div class="text-center py-2.5 rounded-lg" :class="buttonClass">
-          인증하기
+        <div
+          class="text-center py-2.5 rounded-lg"
+          :class="buttonClass"
+          @click="deleteUserAccount()"
+        >
+          탈퇴하기
         </div>
         <div
           class="text-everly-main bg-everly-light_blue text-center py-2.5 rounded-lg hidden md:block"
@@ -93,17 +156,24 @@
 </template>
 
 <script setup lang="ts">
-import modalList from "../components/modalList.vue";
-import dropdownVue from "@/components/common/dropdown.vue";
 import { useMediaQuery } from "@vueuse/core";
 import { ref, computed, watch, onMounted, onUnmounted } from "vue";
 import { useRouter } from "vue-router";
 import { usemypageStore } from "@/store/modules/mypage/mypageStore";
-import { storeToRefs } from "pinia";
 
 const emit = defineEmits(["update:propsShowModal"]);
 const mypageStore = usemypageStore();
 const router = useRouter();
+const reasonList = [
+  "서비스의 오류가 많음",
+  "사용하기 불편함",
+  "거래글 갯수가 적음",
+  "다른 서비스가 더 좋음",
+  "사용빈도가 낮음",
+  "개인정보가 걱정됨",
+  "물품이 안팔림",
+  "사용 빈도가 너무 높음",
+];
 
 //사이즈 확인
 const minSize = computed(() => {
@@ -129,10 +199,67 @@ function goBack() {
 }
 
 //탈퇴하기 버튼 상태
-const buttonClass = ref("bg-everly-mid_grey text-everly-white");
+const buttonClass = ref(
+  "bg-everly-mid_grey text-everly-white cursor-not-allowed"
+);
 
 //최종 탈퇴 선택지
-const withdrawlConfirm = ref("");
+const withdrawlConfirm = ref(false);
+const checkedReason = ref([]); // 선택도니 탈퇴 사유 리스트
+
+const etcContent = ref(""); // 기타 내용
+const etcContentLength = ref(0); //기타 글 길이
+
+watch(etcContent, (text) => {
+  etcContentLength.value = text.length;
+});
+const isActiveEtc = ref(false); // 기타 활성화 여부
+
+// 텍스트에어리어 색깔
+const textareaClass = (status: boolean) => {
+  if (status) return "";
+  else return "bg-everly-wbg_grey";
+};
+
+function inserttoetcContent(event: Event) {
+  var limit = 200;
+  if ((event.target as HTMLInputElement).value.length <= limit) {
+    etcContent.value = (event.target as HTMLInputElement).value;
+  } else etcContent.value = etcContent.value.substring(0, limit);
+}
+
+//탈퇴하기 활성화
+watch([withdrawlConfirm, checkedReason, etcContent, isActiveEtc], () => {
+  if (
+    withdrawlConfirm.value &&
+    (checkedReason.value.length > 0 ||
+      etcContent.value.length > 0 === isActiveEtc.value) //xnor 연산 둘다 true 거나 false 일때만 true
+  ) {
+    buttonClass.value = "bg-everly-main text-everly-white cursor-pointer";
+  } else {
+    buttonClass.value =
+      "bg-everly-mid_grey text-everly-white cursor-not-allowed";
+  }
+});
+
+//탈퇴
+function deleteUserAccount() {
+  if (buttonClass.value == "bg-everly-main text-everly-white cursor-pointer") {
+    var reasonString = checkedReason.value.toString();
+
+    if (etcContent.value.length > 0 && isActiveEtc.value)
+      reasonString += "," + etcContent.value;
+
+    mypageStore.deleteUserAccount(reasonString).then((res) => {
+      if (res) {
+        alert("회원 탈퇴가 완료되었습니다.");
+        router.push("/logout");
+      } else {
+        alert("회원 탈퇴에 실패했습니다.");
+      }
+    });
+  }
+}
 </script>
 
 <style scoped>
@@ -186,7 +313,7 @@ label span:after {
   height: 20px;
   position: absolute;
   left: 0px;
-  bottom: 0px;
+  bottom: -1px;
   background-size: cover;
   opacity: 0;
   vertical-align: middle;
@@ -197,10 +324,8 @@ label span:after {
     height: 15px;
     vertical-align: middle;
   }
-}
-@media (max-width: 768px) {
+
   label span:after {
-    top: 2px;
     width: 15px;
     height: 15px;
     vertical-align: middle;
