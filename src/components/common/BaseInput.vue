f0f04a2c71b47c4426a7a59aebdad30e9a645809
gwangho-y <yigwangho21@gmail.com>
2023-02-09 18:26:55 +0900

    [A] BaseInput추가

---
+++ src/components/common/BaseInput.vue
@@ -0,0 +1,24 @@
+<!--  v-model 한글 인식 못하는 문제를 수정하기 위함 이걸로 input 대신해서 쓰면 -->
+
+<template>
+    <input v-bind:value="value" v-on:input="updateInput">
+  </template>
+<script lang="ts" setup>
+    
+const props = defineProps<{
+    value: string;
+}>();
+const emit = defineEmits(["update:input"]);
+
+function updateInput(event:Event) {
+    const value = event.target as HTMLInputElement;
+    const input = value.value;
+    emit('update:input', input);
+}
+
+
+
+</script>
+<style lang="">
+    
+</style>
